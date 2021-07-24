f508a6c26588c1808d7ad6113fc2d64cb438b225
tamaratsymbaliuk <tsymbalt14@gmail.com>
2021-07-23 20:37:04 -0500

    SupplierInterface

--- FP-02-BuiltIn-Functional-Interface/src/supplierInterface/SupplierInterface.java
+++ FP-02-BuiltIn-Functional-Interface/src/supplierInterface/SupplierInterface.java
@@ -2,5 +2,15 @@ package supplierInterface;
 
 public class SupplierInterface {
 
-    //Supplier has a single abstract method called GET and it accept
+    //Supplier has a single abstract method called GET and it accept no parameter and return type T
+
+    @FunctionalInterface
+    interface Supplier<String>{
+
+        String get();
+
+    }
+
+    Supplier<String > providedStr = ()-> new String("Hello Tamara!");
+
 }
